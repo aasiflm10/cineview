50db2eba89285e69b86d04d7526a17f6dd43615a
aasiflm10 <aa6125405@gmail.com>
2024-11-09 13:04:43 +0530

    added navbar

---
+++ app/components/navbar.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import Link from "next/link";
+
+export function Navbar() {
+  return (
+    <div className="bg-gradient-to-b from-black w-full h-44 pl-24 pr-24 pt-6 pb-25 ">
+      <div className="h-9 flex justify-between">
+        <Link href={"/"}>Hey there</Link>
+        <div className="flex">
+          <label className=" p-4 inline-flex items-center mb-5 cursor-pointer">
+            <input type="checkbox" value="" className="sr-only peer" />
+            <div className="relative w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+          </label>
+
+          <span className="ml-8 "> Profile</span>
+        </div>
+      </div>
+    </div>
+  );
+}
